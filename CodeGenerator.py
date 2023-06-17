3df29f17448f7ab2046b75c4daa48f3b1b654c65
DancingRicardo <99092565+DancingRicardo@users.noreply.github.com>
2023-06-16 21:04:54 -0400

    basic gui func

--- CodeGenerator.py
+++ CodeGenerator.py
@@ -1,27 +1,57 @@
 
 import Field
-import sys
+import math
+import time
+import pygame as pg
 
 class CodeGenerator(): # In Constructor: File Path, Bot Starting X, Y, and Rotation
 
     isDriverRoute = False
 
-    x = 0
-    y = 0
+    x = 100
+    y = 100
 
     botWidth = 0
     botLength = 0
 
+    mainSurfaceArg = pg.Surface((0,0))
+
     botStartingRotation = 0
+
+    lineWidth = 10
+
+    lineDefaultColor = (72,209,204)
+    reverseColor = (235, 252, 252)
+
+    Action1 = (90, 34, 224)
+    Action2 = (11, 127, 222)
+    Action3 = (176, 11, 222)
+    Action4 = (147, 191, 237)
+    Action5 = (245, 226, 22)
+    Action6 = (245, 122, 22)
+
+    currentColor = lineDefaultColor
 
     code = ""     # If Auton, write 0 at the start, if Driver write 1 at the start
 
-    
     ##########   Format   ##########
 
     # { X, Y, Theta, Action Code } #
 
     ################################
+
+    def __init__(self, mainSurfaceArg, width, length, startingRotRad, isDriverArg):
+
+        self.mainSurfaceArg = mainSurfaceArg
+
+        self.botWidth = width
+        self.botLength = length
+
+        self.botStartingRotation = startingRotRad
+
+        self.isDriverRoute = isDriverArg
+
+        print("w")
 
 
     #######   Action Codes   #######
@@ -44,18 +74,85 @@ class CodeGenerator(): # In Constructor: File Path, Bot Starting X, Y, and Rotat
         # Call to field and say that the bot scored, generate code for scoring mechanism.
 
         print("filler")
+        
+    def switchAction(self, eventType):
 
-    def moveTo(xarg, yarg):
+        if (eventType == pg.K_c):
+            self.currentColor = self.Action1
+            print("Reverse")
+            
+        match eventType:
+            case pg.K_x:
+                self.currentColor = self.lineDefaultColor
+                print("Default")
+            case pg.K_c:
+                self.currentColor = self.reverseColor
+                print("Reverse")
+            case pg.K_1:
+                self.currentColor = self.Action1
+                print("Action1")
+            case pg.K_2:
+                self.currentColor = self.Action2
+                print("Action2")
+            case pg.K_3:
+                self.currentColor = self.Action3
+                print("Action3")
+            case pg.K_4:
+                self.currentColor = self.Action4
+                print("Action4")
+            case pg.K_5:
+                self.currentColor = self.Action5
+                print("Action5")
+            case pg.K_6:         
+                self.currentColor = self.Action6
+                print("Action6")
 
-        x = xarg
-        y = yarg
+    def moveTo(self, mousex, mousey, clicked): 
+            
+        # sqrt( (x2 - x1)^2 + (y2 - y1)^2 )
+        radius = math.sqrt( math.pow((self.x - mousex),2) + math.pow((self.y - mousey),2) )        
 
-        theta = 0
-        radius = 0
+        pg.draw.circle(self.mainSurfaceArg, self.currentColor, ( self.x + self.botWidth/2, 
+            self.y + self.botLength/2 ), radius, self.lineWidth)
+
+        pg.draw.line(self.mainSurfaceArg, self.currentColor, (self.x+self.botWidth/2,self.y +
+            self.botLength/2), (mousex, mousey), self.lineWidth) # NEED TO FIX, ADD END COORD
+
+        theta = -math.atan2(( mousey - self.y) , ( mousex - self.x))
+
+        while (theta < 0):
+            theta += 2*math.pi
+
+
+        #liney = math.tan(theta)*radius
+
+        #perpliney = -math.cot(theta)*radius 
+
+        #s = radius*.99
+
+        #b = s*math.cos(theta)*math.cot(theta) + s*math.sin(theta) 
+        
+        #y = -math.cot(theta)*radius + b
+
+        #x = math.sqrt(1-math.pow(y, 2))
+
+
+
+        # 2 different circles, one with the radius arrow
+
+        #pg.draw.polygon(self.mainSurfaceArg, self.lineColor, (coordinateLeftArrow, 
+        #    coordinateRightArrow, coordinateMousePos), self.lineWidth)
 
         # Move to X Position and Y position. Including turn to angle. Find theta by doing trig. Angle between the current mouse pos 
             # and the robot default angle. Radius by how far the mouse is from the robot center. Radius is just for visual purposes.
             # Add an arrow as well pointing away from the bot towards the mouse. Append code to CODE variable. Add way to reverse drive
+
+        if (clicked):
+
+            self.x = mousex
+            self.y = mousey
+
+            
 
     def saveToProject():
 
@@ -64,8 +161,12 @@ class CodeGenerator(): # In Constructor: File Path, Bot Starting X, Y, and Rotat
         print("Saved!")
 
     
+    def update(self):
 
+        drew = pg.draw.rect(self.mainSurfaceArg, (255, 0, 0), (self.x, self.y,
+            self.botWidth, self.botLength))
 
+        return drew
 
 
 
--- main.py
+++ main.py
@@ -1,10 +1,79 @@
 
-class DF():
-    def func():
-        print("\nbruv this is a class right mate")
+import pygame as pg
+import CodeGenerator 
 
-df = DF
+pg.init()
 
-df.func()
+if (not pg.get_init()):
+
+    print("Failed to initialize!")
+
+    # EXIT PROGRAM
+
+screenWidth = 900
+screenHeight = 650
+mainSurface = pg.display.set_mode([ screenWidth, screenHeight ]) # Start up the screen
+
+mainSurface.fill((100, 0, 100))
+
+pg.display.set_caption("Auton Maker V2 - UvuvLib") # Set title of app
+
+pg.display.flip()
+
+# pg.display.toggle_fullscreen()
+
+fieldPic = pg.image.load("overunderfield.jpg").convert() # Loads image, and then converts it to to have a 
+    # consistent pixel format (higher speed)
+
+fieldRec = fieldPic.get_rect()
+fieldRec.center = screenWidth//2, screenHeight//2
+
+cg = CodeGenerator.CodeGenerator(mainSurface, 13.5, 17.5, 0, False)
+
+keepGameRunning = True
+
+while keepGameRunning:
+    
+    hasClicked = False
+
+    for event in pg.event.get():
+        if event.type == pg.QUIT:   
+           keepGameRunning = False
+        if event.type == pg.K_ESCAPE:
+           keepGameRunning = False
+        if event.type == pg.MOUSEBUTTONUP:
+            hasClicked = True
+        cg.switchAction(event.type)
+            
+        
+
+        
+        
+    mainSurface.fill((100, 0, 100))
+
+    mainSurface.blit(fieldPic, fieldRec) # Display image on the screen
+
+    cg.moveTo(pg.mouse.get_pos()[0], pg.mouse.get_pos()[1], hasClicked)
+
+    cg.update()
+
+    pg.display.flip() # HAVE AFTER ALL GRAPHICS UPDATES HAVE FINISHED
+
+
+
+
+
+# while (1):
+
+    # cg.moveTo(1, 1)
+
+    # x = pg.mouse.get_pos()
+
+    # pg.display.update()
+        
+    # print(x)
+
+
+
 
 
